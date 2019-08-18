076204482600cc7ca51ae94cbff47724fd5068b0
Steven Inouye <steveninouye@msn.com>
2019-08-17 20:01:05 -0700

    Complete Time Complexity prompt

--- homework_prompts/01_complexity.js
+++ homework_prompts/01_complexity.js
@@ -14,19 +14,18 @@ Order of Magnitude
 
 Reduce the following into it's Big-O order of magnitude.
 
-1. 5 + N                    Answer:
-2. N + N^2                  Answer:
-3. 15N + 13N                Answer:
-4. 10000                    Answer:
-5. log(N) + 1               Answer:
-6. log(N) * 3 + 14N + 3     Answer:
-7. Nlog(N) + 3N^2           Answer:
-8. N^3 + log(N^4)           Answer:
-9. N! + 180000N^2           Answer:
-10. 15002^N                 Answer:
+1. 5 + N                    Answer: O(N)
+2. N + N^2                  Answer: N^2
+3. 15N + 13N                Answer: O(N)
+4. 10000                    Answer: O(1)
+5. log(N) + 1               Answer: log(N)
+6. log(N) * 3 + 14N + 3     Answer: O(N)
+7. Nlog(N) + 3N^2           Answer: N^2
+8. N^3 + log(N^4)           Answer: N^3
+9. N! + 180000N^2           Answer: N!
+10. 15002^N                 Answer: O(15002^N)
 
 */
-
 
 /*
  *  List the time and auxiliary space complexity of each of the following functions.
@@ -48,12 +47,12 @@ Reduce the following into it's Big-O order of magnitude.
  *     [9, 83, 74], 8 --> -1
  *     [6, 4, 7, 9, 7, 8, 2, 4, 3], 7 --> 2
  *
- *     Time Complexity:
- *     Auxiliary Space Complexity:
+ *     Time Complexity: O(N)
+ *     Auxiliary Space Complexity: O(1)
  */
 
 function indexOf(arr, target) {
-  for (let i = 0; i < arr.length; i ++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
       return i;
     }
@@ -75,13 +74,13 @@ function indexOf(arr, target) {
  *     [9, 83, 74] --> [74]
  *     [6, 4, 7, 9, 7, 8, 2, 4, 3] --> [6, 4, 8, 2, 4]
  *
- *     Time Complexity:
- *     Auxiliary Space Complexity:
+ *     Time Complexity: O(N)
+ *     Auxiliary Space Complexity: O(N)
  */
 
 function evens(arr) {
   var results = [];
-  for (let i=0; i < arr.length; i ++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       results.push(arr[i]);
     }
@@ -104,18 +103,17 @@ function evens(arr) {
  *    'hello' --> ['h', 'e', 'l', 'l', 'o']
  *    'foo' --> ['f', 'o'. 'o']
  *
- *    Time Complexity:
- *    Auxiliary Space Complexity:
+ *    Time Complexity: O(N)
+ *    Auxiliary Space Complexity: O(N)
  */
 
-function split(string){
+function split(string) {
   let results = [];
-  for(let i = 0; i < string.length; i ++){
+  for (let i = 0; i < string.length; i++) {
     results.push(string[i]);
   }
   return results;
 }
-
 
 /*
  *    Sum
@@ -131,13 +129,13 @@ function split(string){
  *    [0, 1, -1] --> 0
  *    [] --> 0
  *
- *    Time Complexity:
- *    Auxiliary Space Complexity:
+ *    Time Complexity: O(N)
+ *    Auxiliary Space Complexity: O(1)
  */
 
 function sum(arr) {
   let total = 0;
-  for(let i = 0; i < arr.length; i ++) {
+  for (let i = 0; i < arr.length; i++) {
     total += arr[i];
   }
   return total;
@@ -158,8 +156,8 @@ function sum(arr) {
  *   [12, 25, 40], [20, 37, 45] --> [12, 20, 25, 37, 40, 45]
  *   [10, 13, 24], [12, 35] --> [10, 12, 13, 24, 35]
  *
- *   Time Complexity:
- *   Auxiliary Space Complexity:
+ *   Time Complexity: O(N + M)
+ *   Auxiliary Space Complexity: O(N + M)
  */
 
 function merge(arr1, arr2) {
@@ -168,7 +166,7 @@ function merge(arr1, arr2) {
   let i = 0;
   let j = 0;
   while (i + j < totalElements) {
-    if (j >= arr2.length || (i < arr1.length && arr1[i] <= arr2[j])){
+    if (j >= arr2.length || (i < arr1.length && arr1[i] <= arr2[j])) {
       result.push(arr1[i]);
       i++;
     } else {
@@ -178,7 +176,6 @@ function merge(arr1, arr2) {
   }
   return result;
 }
-
 
 /*
  *    Counting Sort
@@ -196,18 +193,17 @@ function merge(arr1, arr2) {
  *    [8, 1, 3, 4, 6, 9] --> [1, 3, 4, 6, 8, 9]
  *    [4, 0, 1, 5, 6] --> [0, 1, 4, 5, 6]
  *
- *   Time Complexity:
- *   Auxiliary Space Complexity:
+ *   Time Complexity: O(N^2)
+ *   Auxiliary Space Complexity: O(N)
  */
-
 
 function countingSort(arr, min, max) {
   let count = new Array(max - min + 1).fill(0);
-  for (i = 0; i < arr.length; i ++) {
-    count[arr[i] - min] = count[arr[i] - min] + 1
+  for (i = 0; i < arr.length; i++) {
+    count[arr[i] - min] = count[arr[i] - min] + 1;
   }
   let index = 0;
-  for (let i = 0; i < max + 1; i ++ ) {
+  for (let i = 0; i < max + 1; i++) {
     while (count[i - min] > 0) {
       arr[index] = i;
       index += 1;
@@ -216,7 +212,6 @@ function countingSort(arr, min, max) {
   }
   return arr;
 }
-
 
 /*
  *    Binary Search
@@ -234,8 +229,8 @@ function countingSort(arr, min, max) {
  *    [5, 7, 10, 12, 14], 7 --> 1
  *    [2, 4, 8, 9, 15], 3 --> -1
  *
- *   Time Complexity:
- *   Auxiliary Space Complexity:
+ *   Time Complexity: log(N)
+ *   Auxiliary Space Complexity: O(1)
  */
 
 function binarySearch(arr, val) {
@@ -243,19 +238,16 @@ function binarySearch(arr, val) {
   let high = arr.length - 1;
   while (low <= high) {
     mid = parseInt((low + high) / 2);
-    if(arr[mid] > val) {
+    if (arr[mid] > val) {
       high = mid - 1;
-    }
-    else if (arr[mid] < val) {
+    } else if (arr[mid] < val) {
       low = mid + 1;
-    }
-    else {
+    } else {
       return mid;
     }
   }
   return -1;
 }
-
 
 /*
  *    Factorial
@@ -271,8 +263,8 @@ function binarySearch(arr, val) {
  *    1 --> 1 (1)
  *    9 --> 362880 (9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
  *
- *    Time Complexity:
- *    Auxiliary Space Complexity:
+ *    Time Complexity: O(N)
+ *    Auxiliary Space Complexity: O(N)
  */
 
 function factorial(n) {
@@ -282,95 +274,98 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-
 /*
  *  First Times Last
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(1)
+ *  Auxiliary Space Complexity: O(1)
  */
 
 function firstTimesLast(arr) {
   let result = null;
-  if (arr.length < 2) { return result; }
+  if (arr.length < 2) {
+    return result;
+  }
   result = arr[0] * arr[arr.length - 1];
   return result;
 }
-
 
 /*
  *  Most Frequent Occurrence
  *  NOTE: The string only contains letters in it
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N)
+ *  Auxiliary Space Complexity: O(N)
  */
 
- function mostFrequentOccurrence(str) {
-   const lowerString = str.toLowerCase();
-   let letters = {};
-   let mostFrequent;
+function mostFrequentOccurrence(str) {
+  const lowerString = str.toLowerCase();
+  let letters = {};
+  let mostFrequent;
 
-   for (let i = 0; i < lowerString.length; i++) {
-     let char = str[i].toLowerCase();
-     if (letters[char] ) {
-       letters[char]++;
-     } else {
-       letters[char] = 1;
-     }
-   }
+  for (let i = 0; i < lowerString.length; i++) {
+    let char = str[i].toLowerCase();
+    if (letters[char]) {
+      letters[char]++;
+    } else {
+      letters[char] = 1;
+    }
+  }
 
-   for(let key in letters) {
-     if (!mostFrequent || letters[key] > mostFrequent[1]) {
-       mostFrequent = [key, letters[key]];
-     }
-   }
+  for (let key in letters) {
+    if (!mostFrequent || letters[key] > mostFrequent[1]) {
+      mostFrequent = [key, letters[key]];
+    }
+  }
 
-   return mostFrequent[0];
- }
-
+  return mostFrequent[0];
+}
 
 /*
  *  Print Unordered Pairs
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N^2)
+ *  Auxiliary Space Complexity: O(1)
  */
 
 function printUnorderedPairs(array) {
- for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
-      console.log(array[i] + ',' + array[j]);
+      console.log(array[i] + "," + array[j]);
     }
   }
 }
 
-
 /*
  *  Sorted Array Search
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(logN)
+ *  Auxiliary Space Complexity: O(logN)
  */
 
 function sortedArraySearch(sortedArray, target) {
   function hunt(start, end) {
-    if (start > end) { return false; }
+    if (start > end) {
+      return false;
+    }
     const mid = Math.floor((start + end) / 2);
-    if (sortedArray[mid] === target) { return true; }
-    if (sortedArray[mid] > target){ return hunt(start, mid - 1); }
+    if (sortedArray[mid] === target) {
+      return true;
+    }
+    if (sortedArray[mid] > target) {
+      return hunt(start, mid - 1);
+    }
     return hunt(mid + 1, end);
   }
 
   return hunt(0, sortedArray.length - 1);
 }
 
-
 /*
  *  Make Combined Matrix
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N) with N is the amount of elements in the matrix
+ *  Auxiliary Space Complexity: O(N^2)
  */
 
 function makeCombinedMatrix(arr1, arr2) {
@@ -390,23 +385,23 @@ function makeCombinedMatrix(arr1, arr2) {
 /*
  *  Is There Cat
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(1)
+ *  Auxiliary Space Complexity:  O(1)
  */
 
 function isThereCat(obj) {
   while (obj.cat) {
-    console.log('There is cat!');
-    delete obj.cat; }
-  console.log('There is no cat!');
+    console.log("There is cat!");
+    delete obj.cat;
+  }
+  console.log("There is no cat!");
 }
-
 
 /*
  *  Power of Threes
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(1)
+ *  Auxiliary Space Complexity: O(1)
  */
 
 function powerOfThrees(int) {
@@ -422,12 +417,11 @@ function powerOfThrees(int) {
   return result;
 }
 
-
 /*
  *  Find Duplicate
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N^2)
+ *  Auxiliary Space Complexity: O(1)
  */
 
 function findDuplicate(collection) {
@@ -445,12 +439,11 @@ function findDuplicate(collection) {
   return false;
 }
 
-
 /*
  *  Intersection Points
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N * M)
+ *  Auxiliary Space Complexity: O(N * M)
  */
 function intersectionPoints(arr1, arr2) {
   let result = [];
@@ -468,48 +461,55 @@ function intersectionPoints(arr1, arr2) {
   return result;
 }
 
-
 /*
  *  Nth Fibonacci
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N)
+ *  Auxiliary Space Complexity: O(N)
  */
 function nthFibonacci(n) {
-  let result = [0,1];
+  let result = [0, 1];
   for (let i = 1; i < n; i++) {
     result[i + 1] = result[i] + result[i - 1];
   }
   return result[n];
 }
 
-
 /**
  *  Find Duplicate
  *
  *  Problem: Refactor findDuplicate such that it now finds and returns all
  *           repeating elements in O(N) time complexity.
- *  Auxiliary Space Complexity:
+ *  Auxiliary Space Complexity: O(N)
  */
 
 function findDuplicate(arr) {
-//YOUR WORK HERE
+  const store = {};
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    if (store[el]) return true;
+    store[el] = true;
+  }
+  return false;
 }
-
 
 /*
  *  nth Fibonacci
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(N)
+ *  Auxiliary Space Complexity: O(N)
  */
 
 function nthFibonacci(n) {
   let cache = {};
 
   function searchFib(index) {
-    if (cache[index]) { return cache[index]; }
-    if (index < 2) { return index; }
+    if (cache[index]) {
+      return cache[index];
+    }
+    if (index < 2) {
+      return index;
+    }
     let result = searchFib(index - 1) + searchFib(index - 2);
     cache[index] = result;
     return cache[index];
